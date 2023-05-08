2fb83012528a159d77142306c1e1f28462ffb1d6
Stanislav <76952027+Av3lle@users.noreply.github.com>
2023-05-08 11:31:17 +0300

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -1,4 +1,3 @@
-read $USERNAME
 echo -n "Хотите установить пакеты для игр? (Y/n): "
   read GAMES_PACKAGE
   if [[ $GAMES_PACKAGE == y ]] || [[ $GAMES_PACKAGE == Y ]] || [[ $GAMES_PACKAGE == yes ]] || [[ $GAMES_PACKAGE == YES ]] || [[ $GAMES_PACKAGE == Yes ]]; then
@@ -16,7 +15,7 @@ echo -n "Хотите установить пакеты для игр? (Y/n): "
 
     echo "Идет установка yay..."
     pacman -S --needed --noconfirm git
-    sed -i s/"$USERNAME ALL=(ALL) ALL"/"$USERNAME ALL=(ALL) NOPASSWD: ALL"/g /etc/sudoers.d/$USERNAME
+    sed -i s/"%wheel ALL=(ALL:ALL) ALL"/"%wheel ALL=(ALL:ALL) NOPASSWD: ALL"/g /etc/sudoers
     cd "/home/${USERNAME}" && sudo -u $USERNAME git clone https://aur.archlinux.org/yay.git && cd yay
     sudo -u $USERNAME makepkg -sri --needed --noconfirm
     cd .. && rm -rf yay
@@ -32,7 +31,7 @@ echo -n "Хотите установить пакеты для игр? (Y/n): "
     echo "MESA_GL_VERSION_OVERRIDE=4.5" | tee -a /etc/environment
     echo "MESA_GLSL_VERSION_OVERRIDE=450" | tee -a /etc/environment
     
-    sed -i s/"$USERNAME ALL=(ALL) NOPASSWD: ALL"/"$USERNAME ALL=(ALL) ALL"/g /etc/sudoers.d/$USERNAME
+    sed -i s/"%wheel ALL=(ALL:ALL) NOPASSWD: ALL"/"%wheel ALL=(ALL:ALL) ALL"/g /etc/sudoers
   else
     :
   fi
